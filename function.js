e418e3494c9f8859461c6aac35555c7a1bd27193
Masatoshi <msts.oo0131@gmail.com>
2021-09-16 11:39:25 +0900

    BackgroundColor_changed

---
+++ function.js
@@ -0,0 +1,9 @@
+$(function(){
+    $('a[href^="#"]').click(function(){
+        let href= $(this).attr("href");
+        let target = $(href == "#" || href == "" ? 'html' : href);
+        let position = target.offset().top;
+        $("html, body").animate({scrollTop:position}, 600, "swing");
+        return false;
+    });
+})
