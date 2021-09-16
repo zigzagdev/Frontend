958b04be96d726b9952220f4a7230181f64b53e5
Masatoshi <msts.oo0131@gmail.com>
2021-09-16 14:16:54 +0900

    a_href_path_create

--- function.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    $('a[href^="#"]').click(function(){
-        let href= $(this).attr("href");
-        let target = $(href == "#" || href == "" ? 'html' : href);
-        let position = target.offset().top;
-        $("html, body").animate({scrollTop:position}, 600, "swing");
-        return false;
-    });
-})
